1910a89800dc42b86ed0502ee60da668552f32c3
Abdulrahmon <90264315+abdulrahmontim@users.noreply.github.com>
2026-09-19 08:49:06 +0100

    Time: 107 ms (39.22%), Space: 19.6 MB (94.48%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,48 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        left = 0
+
+        min_len = float("infinity")
+        min_chars = ""
+        min_start = 0
+
+        need = dict()
+        for char in t:
+            need[char] = need.get(char, 0) + 1
+        
+        required = len(t)
+
+        for right in range(len(s)):
+            right_char = s[right]
+
+            if right_char in need:
+                if need[right_char] > 0:
+                    required -= 1
+
+                need[right_char] -= 1
+        
+            while required == 0:
+                # there's a valid window
+                #pop from left while remaining valid, popping means need[char] increases
+                # if need[char] > 0 => needs to stop, renders the window in valid
+                curr_window_len = (right - left) + 1
+                if curr_window_len < min_len:
+                    min_len = curr_window_len
+                    min_chars = s[left:right+1]
+                
+                left_char = s[left]
+                if left_char in need:
+                    need[left_char] += 1
+                    if need[left_char] > 0:
+                        required += 1
+                
+                left += 1
+
+
+        return min_chars
+
+
+
+
+
+
